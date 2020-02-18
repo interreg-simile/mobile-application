6d4956e14383490c8e63172eee0ed4ce349d3d16
Edoardo Pessina <edoardo.pessina@polimi.it>
2020-02-18 11:32:49 +0100

    Outlets modal except photo

--- frontend/app/src/app/observations/details/outlets/outlets.component.ts
+++ frontend/app/src/app/observations/details/outlets/outlets.component.ts
@@ -27,6 +27,22 @@ export class OutletsComponent implements OnInit {
     private _props: Props = {};
 
 
+    /** Selectable colours. */
+    private _colours = {
+        1: { selected: false, colour: "#D64818" },
+        2: { selected: false, colour: "#1060B0" },
+        3: { selected: false, colour: "#1F7F16" },
+        4: { selected: false, colour: "#888888" },
+        5: { selected: false, colour: "#6C4B11" },
+        6: { selected: false, colour: "#D7B427" },
+        7: { selected: false, colour: "#FFFFFF" }
+    };
+
+
+    // Utility function to keep the original key order when iterating on an object using ngFor
+    originalOrder = (a, b) => { return 0 };
+
+
     /** @ignore */
     constructor(private modalCtr: ModalController, private obsService: ObservationsService) { }
 
@@ -43,6 +59,10 @@ export class OutletsComponent implements OnInit {
         this._props.signagePhoto         = this.obsService.newObservation.details.outlets.signagePhoto;
         this._props.prodActNearby        = this.obsService.newObservation.details.outlets.prodActNearby;
         this._props.prodActNearbyDetails = this.obsService.newObservation.details.outlets.prodActNearbyDetails;
+
+        // Select the right colour
+        if (this._props.colour) this._colours[this._props.colour].selected = true;
+
     }
 
 
@@ -63,14 +83,22 @@ export class OutletsComponent implements OnInit {
             this.obsService.newObservation.details.outlets.checked = true;
 
             // Save the new values
-            this.obsService.newObservation.details.outlets.inPlace              = this._props.inPlace;
-            this.obsService.newObservation.details.outlets.terminal.code        = this._props.terminal;
-            this.obsService.newObservation.details.outlets.colour.code          = this._props.colour;
-            this.obsService.newObservation.details.outlets.vapour               = this._props.vapour;
-            this.obsService.newObservation.details.outlets.signage              = this._props.signage;
-            this.obsService.newObservation.details.outlets.signagePhoto         = this._props.signagePhoto;
+            this.obsService.newObservation.details.outlets.inPlace       = this._props.inPlace;
+            this.obsService.newObservation.details.outlets.terminal.code = this._props.terminal;
+
+            this.obsService.newObservation.details.outlets.colour.code =
+                this._props.inPlace ? this._props.colour : undefined;
+
+
+            this.obsService.newObservation.details.outlets.vapour = this._props.vapour;
+
+            this.obsService.newObservation.details.outlets.signage      = this._props.signage;
+            this.obsService.newObservation.details.outlets.signagePhoto =
+                this._props.signage ? this._props.signagePhoto : undefined;
+
             this.obsService.newObservation.details.outlets.prodActNearby        = this._props.prodActNearby;
-            this.obsService.newObservation.details.outlets.prodActNearbyDetails = this._props.prodActNearbyDetails;
+            this.obsService.newObservation.details.outlets.prodActNearbyDetails =
+                this._props.prodActNearby ? this._props.prodActNearbyDetails.trim() : undefined;
 
         }
 
@@ -82,5 +110,48 @@ export class OutletsComponent implements OnInit {
         await this.modalCtr.dismiss();
 
     }
+
+
+    /**
+     * Handles a change in the selected colour.
+     *
+     * @param {Object} colour - The selected colour.
+     */
+    onColourClick(colour) {
+
+        // Set the property to undefined
+        this._props.colour = undefined;
+
+        // For each of the possible colours
+        Object.keys(this._colours).forEach(c => {
+
+            // If the colours has not been selected or if it was already selected
+            if (c !== colour.key || (c === colour.key && this._colours[colour.key].selected)) {
+
+                // Deselect the colour
+                this._colours[c].selected = false;
+
+                // Return
+                return;
+
+            }
+
+            // Select the colour
+            this._colours[c].selected = true;
+
+            // Set the property value
+            this._props.colour = colour.key;
+
+        });
+
+    }
+
+
+    onSignagePhotoClick() {
+
+        console.log("Photo click")
+
+    }
+
 
 }
