8302408ab0473438fe5e155b35d129a868c6106a
Edoardo Pessina <edoardopessina@yahoo.it>
2020-03-21 18:21:37 +0100

    Docs

--- backend/api/src/middlewares/set-lng.js
+++ backend/api/src/middlewares/set-lng.js
@@ -13,44 +13,30 @@ import i18next from "i18next";
  */
 export default function (req, res, next) {
 
-    // Set the default language
     let lng = appConf.defaultLng;
 
-    // Extract the accepted languages
     const lngHeader = req.get("Accept-Language");
 
-    // If the header is provided
     if (lngHeader) {
 
-        // Loop through the specified languages
         lngHeader.split(",").some(l => {
 
-            // If the language is supported
             if (appConf.lngs.includes(l.trim())) {
-
-                // Save it
                 lng = l.trim();
-
-                // Return true
                 return true;
-
             }
 
-            // Return false
             return false;
 
         });
 
     }
 
-    // Save the language and the fixed t function in the request object
     req.lng = lng;
     req.t   = i18next.getFixedT(lng);
 
-    // Set the response header
     res.set("Content-Language", lng);
 
-    // Call the next middleware
     next()
 
 }
